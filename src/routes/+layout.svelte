cb7f6d1f0629269c48bfa8ab7c7bc0e6c442fefc
Jonas Stettner <mail@jstet.net>
2023-01-06 02:30:53 +0100

    header improvements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,19 +6,18 @@
 
 <!-- Footer on bottom of page if page is too short -->
 <div class="flex flex-col items-center min-h-screen">
-	<Header/>
-	<div id="grow" class="w-screen px-5 py-5">
+	<Header>
+	<div id="grow" class="w-screen pt-2 px-5">
 		<slot/>
 	</div>
 	<Footer />
+	</Header>
 </div>
 
 <style>
 	#grow {
 		flex: 1 1 auto; /*grow vertically*/
     /* same padding as navbar */
-    padding-left: var(--navbar-padding, 0.5rem);
-    padding-right: var(--navbar-padding, 0.5rem);
 	}
 
 </style>
