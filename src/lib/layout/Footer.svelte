12606eec055d13b11a4ec1f35560ecc107539cef
Jonas Stettner <mail@jstet.net>
2023-01-19 22:13:20 +0100

    added social media icons

--- src/lib/layout/Footer.svelte
+++ src/lib/layout/Footer.svelte
@@ -1,42 +1,80 @@
 <script>
   import { t, locale } from "$lib/stores/i18n.js";
+  import Facebook from "$lib/svg/Facebook.svelte";
+  import Instagram from "../svg/Instagram.svelte";
+  import Twitter from "../svg/Twitter.svelte";
+  import Youtube from "../svg/Youtube.svelte";
+  import Linkedin from "../svg/Linkedin.svelte";
+  import Mastodon from "../svg/Mastodon.svelte";
+
+  const social_media_height = 30;
 </script>
+
 <footer class="w-screen border-t-2 border-neutral-25">
-  <div
-    class=" flex mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 pb-4 pt-5"
-  >
-    <div class="mx-auto block">
-      <ul
-        class="flex items-center gap-6 font-light text-base-content tracking-wide "
+  <div class=" flex pt-4">
+    <ul
+      class="flex items-center gap-6 font-light text-base-content tracking-wide mx-auto"
+    >
+      <li>
+        <a
+          class="hover:text-primary transition"
+          href={$t("footer.imprint").url}
+        >
+          {$t("footer.imprint").text}
+        </a>
+      </li>
+      <li>
+        <a
+          class="hover:text-primary transition"
+          href={$t("footer.contact").url}
+        >
+          {$t("footer.contact").text}
+        </a>
+      </li>
+    </ul>
+  </div>
+
+  <div class=" flex mx-auto py-6">
+    <div class="grid grid-flow-col gap-6 mx-auto">
+      <a
+        href="https://twitter.com/CorrelAid"
+        class=""
+        ><Instagram width={social_media_height} height={social_media_height} /></a
       >
-        <li>
-          <a class="hover:text-primary transition" href={$t("footer.imprint").url}> {$t("footer.imprint").text} </a>
-        </li>
-        <li>
-          <a class="hover:text-primary transition" href={$t("footer.contact").url}> {$t("footer.contact").text} </a>
-        </li>
-      </ul>
+      <a
+        href="https://twitter.com/correlaid"
+        class=""
+        ><Twitter width={social_media_height} height={social_media_height} /></a
+      >
+      <a
+      href="https://masto.ai/@correlaid"
+        class=""
+        ><Mastodon width={social_media_height} height={social_media_height} /></a
+      >
+      <a
+        href="https://de.linkedin.com/company/correlaid"
+        class=""
+        ><Linkedin width={social_media_height} height={social_media_height} /></a
+      >
+      <a
+        href="https://facebook.com/WeAreCorrelAid"
+        class=""
+        ><Facebook width={social_media_height} height={social_media_height} /></a
+      >
+      <a
+        href="https://www.youtube.com/channel/UCs_k9roCuWLy17xxpigrWbg"
+        class=""
+        ><Youtube width={social_media_height} height={social_media_height} /></a
+      >
+      
     </div>
   </div>
-  <div
-    class=" flex mx-auto max-w-screen-xl pb-6 pt-2"
-  >
-    <div class="mx-auto block">
-      <ul
-        class="flex items-center gap-6 font-light text-base-content tracking-wide"
-      >
-        <li>CorrelAid. © {new Date().getFullYear()}</li>
-      </ul>
-      
-    </div>
-  </div>
-  <div
-    class=" flex mx-auto max-w-screen-xl pb-6 pt-2"
-  >
-    <div class="mx-auto block">
-      
-    
-      
-    </div>
+
+  <div class=" flex pb-4">
+    <ul
+      class="flex items-center gap-6 font-light text-base-content tracking-wide mx-auto"
+    >
+      <li>CorrelAid. © {new Date().getFullYear()}</li>
+    </ul>
   </div>
 </footer>
