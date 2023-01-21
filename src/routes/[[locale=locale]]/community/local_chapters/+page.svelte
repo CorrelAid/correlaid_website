1fd68d2e48dd7cd7b3dc3f8396c4309396916588
Jonas Stettner <mail@jstet.net>
2023-01-22 00:28:36 +0100

    working on lc map

--- src/routes/[[locale=locale]]/community/local_chapters/+page.svelte
+++ src/routes/[[locale=locale]]/community/local_chapters/+page.svelte
@@ -1,8 +1,9 @@
 <script>
     import { page_key } from "$lib/stores/page_key.js";
     import { onMount, onDestroy } from "svelte";
-    import { Map } from "maplibre-gl";
-    import germany from '$lib/assets/geojson/germany.json';
+    import { Map, Popup } from "maplibre-gl";
+    import { t, locale, locales } from "$lib/stores/i18n.js";
+    import lc_coords from "$lib/data/lc_coords.json";
     import Content from "$lib/components/Content.svelte";
     import "maplibre-gl/dist/maplibre-gl.css";
 
@@ -13,43 +14,101 @@
     let map;
     let mapContainer;
 
-    const vizData = {
-        "40":{"name":"Austria","population":8932664},
-        "250":{"name":"France","population":67439599},
-        "276":{"name":"Germany","population":83155031},
-        "528":{"name":"Netherlands","population":17475415},
-        "756":{"name":"Switzerland","population":8667088}
-      };
-
-    $: console.log(germany)
-
     onMount(async () => {
         const apiKey = "cYwZssWUHS4exn283ZO4";
 
-        const initialState = { lng: 10, lat: 49, zoom: 4 };
+        const initialState = { lng: 5.5, lat: 49, zoom: 4.2 };
 
         map = new Map({
             container: mapContainer,
-            style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${apiKey}`,
+            style: `https://api.maptiler.com/maps/stage-light/style.json?key=${apiKey}`,
             center: [initialState.lng, initialState.lat],
             zoom: initialState.zoom,
+            maxZoom: 11,
+            minZoom: 4,
+            maxBounds: [
+                [-20, 32],
+                [40, 65],
+            ],
         });
         map.on("load", function () {
-            map.addSource("maine", {
+            var layers = map.getStyle().layers;
+            // Find the index of the first symbol layer in the map style
+            var firstSymbolId;
+            for (var i = 0; i < layers.length; i++) {
+                if (layers[i].type === "symbol") {
+                    firstSymbolId = layers[i].id;
+                    break;
+                }
+            }
+            map.addSource("statesData", {
+                type: "vector",
+                url: `https://api.maptiler.com/tiles/countries/tiles.json?key=${apiKey}`,
+            });
+            map.addSource("lcs", {
                 type: "geojson",
-                data: germany
+                data: lc_coords,
             });
 
+            map.addLayer(
+                {
+                    id: "lcs",
+                    type: "circle",
+                    source: "lcs",
+                    paint: {
+                        "circle-radius": 10,
+                        "circle-opacity": 0.7,
+                        "circle-color": "#f04451",
+                    },
+                    layout: {
+                        // "icon-image": "marker",
+                        // get the year from the source's "year" property
+                        // "text-field": ["get", "year"],
+                        // "text-font": [
+                        //     "Open Sans Semibold",
+                        //     "Arial Unicode MS Bold",
+                        // ],
+                        // "text-offset": [0, 1.25],
+                        // "text-anchor": "top",
+                    },
+                },
+                firstSymbolId
+            );
+
             map.addLayer({
-                id: "maine",
+                id: "countries",
+                source: "statesData",
                 type: "fill",
-                source: "maine",
-                layout: {},
+                "source-layer": "administrative",
+                filter: [
+                    "all",
+                    ["==", "level", 0],
+                    [
+                        "any",
+                        ["==", "name", "Germany"],
+                        ["==", "name", "Switzerland"],
+                        ["==", "name", "France"],
+                        ["==", "name", "Netherlands"],
+                        ["==", "name", "Austria"],
+                    ],
+                ],
                 paint: {
-                    "fill-color": "#088",
-                    "fill-opacity": 0.8,
+                    "fill-color": "#df595b",
+                    "fill-opacity": 0.2,
                 },
             });
+        });
+
+        map.on("click", "lcs", (e) => {
+            const lcs = e.features[0];
+
+            new Popup()
+                .setHTML(
+                    `<b>${lcs.properties.year}</b>`
+                )
+
+                .setLngLat(lcs.geometry.coordinates)
+                .addTo(map);
         });
     });
     onDestroy(() => {
@@ -57,6 +116,33 @@
             map.remove();
         }
     });
+
+    $: console.log($locale);
+
+    $: if (map) {
+        if (map.isStyleLoaded()) {
+            map.setLayoutProperty("continent", "text-field", [
+                "get",
+                "name:" + $locale,
+            ]);
+            map.setLayoutProperty("country", "text-field", [
+                "get",
+                "name:" + $locale,
+            ]);
+            map.setLayoutProperty("state", "text-field", [
+                "get",
+                "name:" + $locale,
+            ]);
+            map.setLayoutProperty("city", "text-field", [
+                "get",
+                "name:" + $locale,
+            ]);
+            map.setLayoutProperty("town", "text-field", [
+                "get",
+                "name:" + $locale,
+            ]);
+        }
+    }
 </script>
 
 <Content>
@@ -74,6 +160,6 @@
 
 <style>
     #map {
-        height: 500px;
+        height: 600px;
     }
 </style>
