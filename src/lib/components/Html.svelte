6cf19ab9d845f8636052eb4a46789c35e41b177d
Jonas Stettner <mail@jstet.net>
2023-03-10 23:17:15 +0100

    #63 video embedding works, adjusted size inside prose

--- src/lib/components/Html.svelte
+++ src/lib/components/Html.svelte
@@ -4,8 +4,9 @@
     export let width;
 </script>
 
+<!-- see app.css for more prose adjustments -->
 <article
-    class="prose max-w-none container mx-auto px-4 text-neutral prose-h1:text-3xl  prose-h1:text-neutral prose-h2:text-neutral {options} {width == "text" ? "text_width" : ""}"
+    class="prose max-w-none container mx-auto px-4 text-neutral prose-h1:text-3xl  prose-h1:text-neutral prose-h2:text-neutral prose-iframe: {options} {width == "text" ? "text_width" : ""}"
    
 >
     {@html source}
