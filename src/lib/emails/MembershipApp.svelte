1ed8bcb6e120da9438d93ff2522f1f54cfa3fa8d
Jonas Stettner <mail@jstet.net>
2023-04-12 12:03:10 +0200

    #94 templating works

--- src/lib/emails/MembershipApp.svelte
+++ src/lib/emails/MembershipApp.svelte
@@ -1,31 +1,17 @@
 <script>
 	import { Button, Container, Head, Hr, Html, Img, Preview, Section, Text } from 'svelte-email';
 
+	export let name = "ddf";
+
 	
 </script>
 
-<Html lang="en">
+<Html lang="de">
 	<Head />
 	<Preview preview="Welcome to svelte-email" />
 	<Section>
 		<Container>
-			<Img
-				src="https://svelte.dev/svelte-logo-horizontal.svg"
-				alt="Svelte logo"
-			
-				width="200"
-				height="50"
-			/>
-			<Text>, welcome to svelte-email</Text>
-			<Text>A Svelte component library for building responsive emails</Text>
-			<Section >
-				<Button pX={12} pY={12} href="https://github.com/carstenlebek/svelte-email">
-					View on GitHub
-				</Button>
-			</Section>
-			<Text>Happy coding!</Text>
-			<Hr/>
-			<Text>Carsten Lebek</Text>
+			<Text>{name} würde gerne Mitglied im Verein werden</Text>
 		</Container>
 	</Section>
 </Html>
--- src/routes/[[locale=locale]]/community/[become_member=become_member]/[membership_application=membership_application]/+page.server.js
+++ src/routes/[[locale=locale]]/community/[become_member=become_member]/[membership_application=membership_application]/+page.server.js
@@ -6,44 +6,48 @@ import { SECRET_GOOGLE_USER } from '$env/static/private'
 import { SECRET_GOOGLE_PW } from '$env/static/private'
 
 
-const transporter = nodemailer.createTransport({
-	service: 'gmail',
-  auth: {
-    user: SECRET_GOOGLE_USER,
-    pass: SECRET_GOOGLE_PW
-  }
-});
 
-// const emailHtml = render({
-// 	component: MembershipApp,
-// 	props: {
-// 		name: 'Svelte'
-// 	}
-// });
-
-const mailOptions = {
-    from: SECRET_GOOGLE_USER,
-    to: "mail@jstet.net",
-    subject: 'Subject',
-    text: "test"
-  };
 
 /** @type {import('./$types').Actions} */
 export const actions = {
     default: async ({ request }) => {
         const data = await request.formData();
-        const email = data.get('email');
 
+        const name =  data.get("name")
+        console.log(name)
 
-        console.log(email)
-
-        transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-           console.log(error);
-            } else {
-              console.log('Email sent: ' + info.response);
-              // do something useful
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: SECRET_GOOGLE_USER,
+                pass: SECRET_GOOGLE_PW
             }
-          });
+        });
+
+        const renda = render({
+
+            template: MembershipApp,
+            props: {
+                name: name
+            }
+
+
+        });
+
+        const options = {
+            from: SECRET_GOOGLE_USER,
+            to: "mail@jstet.net",
+            subject: 'hello world',
+            html: renda
+        };
+
+        transporter.sendMail(options, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+                // do something useful
+            }
+        });
     }
-  };
+};
