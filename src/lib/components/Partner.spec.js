59b82455553a2712ce29d5ef3b068ca7ada74e3c
KonradUdoHannes <konrad.woelms@gmail.com>
2023-04-24 20:26:22 +0200

    added test setup with vitest and first testcases and added it to ci

---
+++ src/lib/components/Partner.spec.js
@@ -0,0 +1,25 @@
+import Partner from './Partner.svelte';
+import {render, screen} from '@testing-library/svelte';
+
+describe('Partner', () => {
+  test('contains name and details', () => {
+    render(Partner, {props: {name: 'NPO A', description: 'a very cool NPO'}});
+
+    expect(screen.getByText('NPO A')).toBeInTheDocument();
+    expect(screen.getByText('a very cool NPO')).toBeInTheDocument();
+  });
+
+  test('image has hyperlink', () => {
+    render(Partner, {
+      props: {
+        name: 'NPO A',
+        description: 'a very cool NPO',
+        img: 'fakeimgurl.org ',
+        website: 'npoa.website.org',
+      },
+    });
+
+    const image = screen.getByAltText('NPO A');
+    expect(image.closest('a')).toHaveAttribute('href', 'npoa.website.org');
+  });
+});
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import {sveltekit} from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/setupTest.js'],
+  },
 };
 
 export default config;
