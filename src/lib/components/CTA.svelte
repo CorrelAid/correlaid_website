a7489454ccd508ec0f391bd31ebff0b30914204b
Jonas Stettner <mail@jstet.net>
2023-04-24 19:00:11 +0200

    init cta component

---
+++ src/lib/components/CTA.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let button_link;
+  export let button_text;
+  export let text;
+</script>
+
+<div class="border bg-gradient-to-r from-primary-75 to-secondary-75 text-white">
+  test
+</div>
--- src/routes/[[locale=locale]]/+page.svelte
+++ src/routes/[[locale=locale]]/+page.svelte
@@ -1,8 +1,11 @@
 <script>
   import {page_key} from '$lib/stores/page_key';
   import {onMount} from 'svelte';
+  import CTA from '$lib/components/CTA.svelte';
 
   onMount(() => {
     $page_key = 'navbar.home';
   });
 </script>
+
+<CTA />
