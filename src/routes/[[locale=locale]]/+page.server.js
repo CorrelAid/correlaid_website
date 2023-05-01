e87a46eb585ee3977cbc6a0c0e2e0c9db62cc62c
KonradUdoHannes <konrad.woelms@gmail.com>
2023-05-01 18:45:24 +0200

    started graphql refactor by implementing graphql variables and moving the queries to seperate files in the corresponding route.

--- src/routes/[[locale=locale]]/+page.server.js
+++ src/routes/[[locale=locale]]/+page.server.js
@@ -1,89 +1,13 @@
 import directus_fetch from '$lib/js/directus_fetch';
 import {handle_lang} from '$lib/js/helpers';
 import {get_lang} from '$lib/js/helpers';
+import {latestUpdatesQuery} from './queries.js';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({params}) {
-  // prettier-ignore
-  const query = `
-  query{
-	Posts( 
-        sort: [ "-pubdate" ]
-    ){
-		pubdate
-    title_image{id}
-      content_creators{
-        Content_Creators_id{
-            person{
-                name
-                translations(
-                  filter: { languages_code: { code: { _eq: "${get_lang(params)}" } } }
-                ) {
-                  pronouns}
-            }
-        }
-    }
-       
-      translations{
-        languages_code{
-          code
-      }
-          title
-          text
-          tags
-          slug
-          teaser
-          
-
-      }
-    }
-    Events(sort: [ "-date" ]) {
-      id
-      date
-      start_time
-      end_time
-      title
-      teaser
-      registration_link
-      target_group
-      language
-      type
-      slug
-      tags
-      local_chapters {
-        Local_Chapters_id {
-          translations(
-            filter: { languages_code: { code: { _eq: "${get_lang(params)}" } } }
-          ) {
-            city
-          }
-        }
-      }
-  
-    }
-   
-      Podcast_Episodes(sort: "-publication_datetime"){
-      title
-      soundcloud_link
-      description
-      language
-      publication_datetime
-      tags
-      content_creators{
-        Content_Creators_id{
-            person{
-                name
-                translations(
-                  filter: { languages_code: { code: { _eq: "${get_lang(params)}" } } }
-                ) {
-                  pronouns}
-            }
-        }
-    }
-    }
-}`
-
-  const data = await directus_fetch(query);
+  const data = await directus_fetch(latestUpdatesQuery, {
+    language: get_lang(params),
+  });
 
   const posts = handle_lang(data.Posts, params);
 
