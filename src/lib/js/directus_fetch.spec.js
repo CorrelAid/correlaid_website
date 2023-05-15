ab7f52e18561516b3b13b4abf8b6bf9158e39b55
KonradUdoHannes <konrad.woelms@gmail.com>
2023-05-15 12:09:07 +0200

    fixed typo in the language variable for the team route and the podcast route. Introduced variable validation for directus_fetch.

---
+++ src/lib/js/directus_fetch.spec.js
@@ -0,0 +1,21 @@
+import {validateVars} from './directus_fetch.js';
+
+describe('variable validation', () => {
+  test("Allowed variabled don't throw error", () => {
+    const exampleVars = {
+      page: 0,
+      language: 1,
+      slug: 2,
+    };
+    validateVars(exampleVars);
+  });
+
+  test('last . seperated grou is extracted by getGroup', () => {
+    const exampleVars = {
+      page: 0,
+      unallowedVariableName: 1,
+      slug: 2,
+    };
+    expect(() => validateVars(exampleVars)).toThrowError();
+  });
+});
