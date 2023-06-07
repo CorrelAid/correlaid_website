81695d9aca2eb5d7c2d94e0c4d8dfb92b9b22f3a
KonradUdoHannes <konrad.woelms@gmail.com>
2023-06-07 13:19:07 +0200

    server side parsing of project overview

--- src/lib/components/ProjectsCard.svelte
+++ src/lib/components/ProjectsCard.svelte
@@ -30,13 +30,13 @@
   <div class="px-4 pb-6 pt-6">
     <div class="mb-2 flex items-center pb-2">
       <Nonprofit width={25} height={25} />
-      <h4 class="text-md ml-2 font-semibold text-primary line-clamp-3">
+      <h4 class="text-md ml-2 line-clamp-3 font-semibold text-primary">
         {organization}
       </h4>
     </div>
 
     <h3
-      class="mb-3 mt-2 block text-xl font-semibold text-base-content transition line-clamp-3"
+      class="mb-3 mt-2 line-clamp-3 block text-xl font-semibold text-base-content transition"
     >
       {title}
     </h3>
@@ -45,7 +45,7 @@
     {#if correlaidx.length !== 0}
       {#each correlaidx as lc}
         <a
-          class="text-medium mb-3 font-semibold text-base-content transition line-clamp-3 hover:text-primary"
+          class="text-medium mb-3 line-clamp-3 font-semibold text-base-content transition hover:text-primary"
           href={gen_lc_href(
             $page.params,
             lc.Local_Chapters_id.translations[0].city,
