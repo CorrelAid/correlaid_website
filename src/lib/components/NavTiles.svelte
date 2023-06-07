9cd11ba94ecdf0f5558104eb2a0a30b4ad4353c8
Jonas Stettner <mail@jstet.net>
2023-06-07 12:13:51 +0200

    asjust padding values page, change value page title

--- src/lib/components/NavTiles.svelte
+++ src/lib/components/NavTiles.svelte
@@ -6,30 +6,40 @@
 </script>
 
 <div
-  class="mx-4 grid grid-cols-[repeat(auto-fit,minmax(144px,max-content))] justify-center gap-8 p-[initial] md:grid-cols-[repeat(auto-fit,minmax(176px,max-content))]"
+  class="mx-4 flex flex-wrap md:grid {navItems.length % 2 == 0
+    ? 'md:grid-cols-2'
+    : 'md:grid-cols-3'} gap-8 md:place-content-center"
 >
   {#each navItems as item, i}
-    <a
-      href={$t(item.key).url}
-      class="flex h-36 w-36 items-center justify-center rounded opacity-80 md:h-44 md:w-44"
-      style="background-color: {colorScale[i]};"
+    <div
+      class="flex items-center {navItems.length % 2 == 0
+        ? i % 2 == 0
+          ? 'md:justify-end'
+          : 'md:justify-start'
+        : 'md:justify-center'}"
     >
-      <div class="flex flex-col items-center justify-center opacity-100">
-        <svelte:component
-          this={item.icon}
-          height={icon_size}
-          width={icon_size}
-          fill={'white'}
-        />
+      <a
+        href={$t(item.key).url}
+        class="flex h-36 w-36 items-center justify-center rounded opacity-80 md:h-44 md:w-44"
+        style="background-color: {colorScale[i]};"
+      >
+        <div class="flex flex-col items-center justify-center opacity-100">
+          <svelte:component
+            this={item.icon}
+            height={icon_size}
+            width={icon_size}
+            fill={'white'}
+          />
 
-        <h2
-          class="break-word mx-2 pt-2 text-center font-londrina text-xl leading-6 tracking-[0.025em] text-white opacity-100 md:text-2xl md:leading-7"
-        >
-          {$t(item.key).text === 'Bildungsressourcen'
-            ? 'Ressourcen'
-            : $t(item.key).text}
-        </h2>
-      </div>
-    </a>
+          <h2
+            class="break-word mx-2 pt-2 text-center font-londrina text-xl leading-6 tracking-[0.025em] text-white opacity-100 md:text-2xl md:leading-7"
+          >
+            {$t(item.key).text === 'Bildungsressourcen'
+              ? 'Ressourcen'
+              : $t(item.key).text}
+          </h2>
+        </div>
+      </a>
+    </div>
   {/each}
 </div>
