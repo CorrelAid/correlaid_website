8faf9a34d9b7d0cda3a6e9dfc6d5d407c28317b6
Jonas Stettner <mail@jstet.net>
2023-06-30 22:14:01 +0200

    add and adjust filter to podcast

--- src/routes/[[locale=locale]]/podcast/+page.svelte
+++ src/routes/[[locale=locale]]/podcast/+page.svelte
@@ -1,21 +1,45 @@
 <script>
   import {page_key} from '$lib/stores/page_key';
   import {onMount} from 'svelte';
+  import {t} from '$lib/stores/i18n';
   import BlogCard from '$lib/components/Blog_Card.svelte';
+  import Filter from '../../../lib/components/Filter.svelte';
 
   onMount(() => {
     $page_key = 'navbar.podcast';
   });
 
   export let data;
-
+  let filter_data;
   $: podcast_episodes = data.podcast_episodes;
+
+  const selects = [
+    {
+      title: $t('filter.language').text,
+      searchable: false,
+      multiple: true,
+      param: 'langs',
+    },
+  ];
+
+  const searchOptions = [
+    {name: 'title', multiple: false},
+    {name: 'teaser', multiple: false},
+  ];
 </script>
 
-<div class="container mx-auto px-4 pb-8">
+<Filter
+  orig_data={podcast_episodes}
+  bind:filter_data
+  {selects}
+  {searchOptions}
+/>
+<div class="container mx-auto mt-8 px-4 pb-8">
   <div class="space-y-8">
-    {#each podcast_episodes as episode}
-      <BlogCard {...episode} external={true} />
-    {/each}
+    {#if filter_data}
+      {#each filter_data as episode}
+        <BlogCard {...episode} external={true} />
+      {/each}
+    {/if}
   </div>
 </div>
