31f5c4e3a31dbddf95e19d52dd55b4c29da3dd13
jstet <mail@jstet.net>
2024-01-12 14:02:50 +0100

    manually gen rss

--- src/routes/[[locale=locale]]/rss.xml/+server.js
+++ src/routes/[[locale=locale]]/rss.xml/+server.js
@@ -1,4 +1,3 @@
-import {Feed} from 'feed';
 import directus_fetch from '$lib/js/directus_fetch';
 import {getAllowedStatus} from '$lib/js/directus_fetch.js';
 import {handle_lang, get_locale} from '$lib/js/helpers';
@@ -7,13 +6,20 @@ import {blogQuery} from './queries.js';
 import {parseEntries} from '$lib/js/parse_cms.js';
 
 export async function GET({params}) {
-  const feed = new Feed({
-    title: 'CorrelAid Blog',
-    id: `https://correlaid.org/${get_locale(params)}`,
-    link: `https://correlaid.org/${get_locale(params)}/rss.xml`,
-    language: `${get_locale(params)}`,
-    copyright: `${new Date().getFullYear()} CorrelAid`,
-  });
+  const feedTitle = 'CorrelAid Blog';
+  const feedLink = `https://correlaid.org/${get_locale(params)}/rss.xml`;
+  const feedLanguage = `${get_locale(params)}`;
+  const feedCopyright = `${new Date().getFullYear()} CorrelAid`;
+
+  let xmlString = `<?xml version="1.0" encoding="UTF-8" ?>
+  <rss version="2.0">
+    <channel>
+      <title>${feedTitle}</title>
+      <link>${feedLink}</link>
+      <description>${feedTitle}</description>
+      <language>${feedLanguage}</language>
+      <copyright>${feedCopyright}</copyright>
+  `;
 
   const data = await directus_fetch(blogQuery, {
     language: get_lang(params),
@@ -25,19 +31,34 @@ export async function GET({params}) {
   const parsed_posts = parseEntries(posts, 'blog_posts');
 
   for (const post of parsed_posts) {
-    feed.addItem({
-      title: post.title,
-      author: post.content_creators
-        .map((creator) => creator.Content_Creators_id.person.name)
-        .filter((name) => name)
-        .join(', '),
-      description: post.teaser,
-      link: `https://correlaid.org${params.locale == 'en' ? '/en' : ''}/blog/${
-        post.slug
-      }`,
-      custom_elements: [{languages: post.langs.join(', ')}],
-    });
+    const title = post.title;
+    const author = post.content_creators
+      .map((creator) => creator.Content_Creators_id.person.name)
+      .filter((name) => name)
+      .join(', ');
+    const description = post.teaser;
+    const link = `https://correlaid.org${
+      params.locale == 'en' ? '/en' : ''
+    }/blog/${post.slug}`;
+    const languages = post.langs.join(', ');
+
+    xmlString += `
+      <item>
+        <title>${title}</title>
+        <author>${author}</author>
+        <description>${description}</description>
+        <link>${link}</link>
+        <languages>${languages}</languages>
+      </item>
+    `;
   }
 
-  return new Response(feed.rss2({indent: true}));
+  xmlString += `
+    </channel>
+  </rss>
+  `;
+
+  return new Response(xmlString, {
+    headers: {'Content-Type': 'application/xml'},
+  });
 }
