a5c65873b4d53aeaf219e55a1d873660227d6343
jstet <mail@jstet.net>
2024-01-12 13:57:57 +0100

    test with feed

--- src/routes/[[locale=locale]]/rss.xml/+server.js
+++ src/routes/[[locale=locale]]/rss.xml/+server.js
@@ -1,4 +1,4 @@
-// import {Feed} from 'feed';
+import {Feed} from 'feed';
 import directus_fetch from '$lib/js/directus_fetch';
 import {getAllowedStatus} from '$lib/js/directus_fetch.js';
 import {handle_lang, get_locale} from '$lib/js/helpers';
@@ -7,13 +7,13 @@ import {blogQuery} from './queries.js';
 import {parseEntries} from '$lib/js/parse_cms.js';
 
 export async function GET({params}) {
-  // const feed = new Feed({
-  //   title: 'CorrelAid Blog',
-  //   id: `https://correlaid.org/${get_locale(params)}`,
-  //   link: `https://correlaid.org/${get_locale(params)}/rss.xml`,
-  //   language: `${get_locale(params)}`,
-  //   copyright: `${new Date().getFullYear()} CorrelAid`,
-  // });
+  const feed = new Feed({
+    title: 'CorrelAid Blog',
+    id: `https://correlaid.org/${get_locale(params)}`,
+    link: `https://correlaid.org/${get_locale(params)}/rss.xml`,
+    language: `${get_locale(params)}`,
+    copyright: `${new Date().getFullYear()} CorrelAid`,
+  });
 
   const data = await directus_fetch(blogQuery, {
     language: get_lang(params),
@@ -24,20 +24,20 @@ export async function GET({params}) {
 
   const parsed_posts = parseEntries(posts, 'blog_posts');
 
-  // for (const post of parsed_posts) {
-  //   feed.addItem({
-  //     title: post.title,
-  //     author: post.content_creators
-  //       .map((creator) => creator.Content_Creators_id.person.name)
-  //       .filter((name) => name)
-  //       .join(', '),
-  //     description: post.teaser,
-  //     link: `https://correlaid.org${params.locale == 'en' ? '/en' : ''}/blog/${
-  //       post.slug
-  //     }`,
-  //     custom_elements: [{languages: post.langs.join(', ')}],
-  //   });
-  // }
+  for (const post of parsed_posts) {
+    feed.addItem({
+      title: post.title,
+      author: post.content_creators
+        .map((creator) => creator.Content_Creators_id.person.name)
+        .filter((name) => name)
+        .join(', '),
+      description: post.teaser,
+      link: `https://correlaid.org${params.locale == 'en' ? '/en' : ''}/blog/${
+        post.slug
+      }`,
+      custom_elements: [{languages: post.langs.join(', ')}],
+    });
+  }
 
-  return new Response(String('hu'));
+  return new Response(feed.rss2({indent: true}));
 }
