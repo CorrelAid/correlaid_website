328804ee79edd29a9a3b239dcc63b9515caf454a
jstet <mail@jstet.net>
2024-01-12 12:42:34 +0100

    switch rss with feed package

--- src/routes/[[locale=locale]]/rss.xml/+server.js
+++ src/routes/[[locale=locale]]/rss.xml/+server.js
@@ -1,4 +1,4 @@
-import RSS from 'rss';
+import {Feed} from 'feed';
 import directus_fetch from '$lib/js/directus_fetch';
 import {getAllowedStatus} from '$lib/js/directus_fetch.js';
 import {handle_lang, get_locale} from '$lib/js/helpers';
@@ -6,12 +6,11 @@ import {get_lang} from '$lib/js/helpers';
 import {blogQuery} from './queries.js';
 import {parseEntries} from '$lib/js/parse_cms.js';
 
-/** @type {import('./$types').RequestHandler} */
 export async function GET({params}) {
-  const feed = new RSS({
+  const feed = new Feed({
     title: 'CorrelAid Blog',
-    site_url: `https://correlaid.org/${get_locale(params)}`,
-    feed_url: `https://correlaid.org/${get_locale(params)}/rss.xml`,
+    id: `https://correlaid.org/${get_locale(params)}`,
+    link: `https://correlaid.org/${get_locale(params)}/rss.xml`,
     language: `${get_locale(params)}`,
     copyright: `${new Date().getFullYear()} CorrelAid`,
   });
@@ -25,19 +24,20 @@ export async function GET({params}) {
 
   const parsed_posts = parseEntries(posts, 'blog_posts');
 
-  for (const post of parsed_posts)
-    feed.item({
+  for (const post of parsed_posts) {
+    feed.addItem({
       title: post.title,
       author: post.content_creators
         .map((creator) => creator.Content_Creators_id.person.name)
         .filter((name) => name)
         .join(', '),
       description: post.teaser,
-      url: `https://correlaid.org${params.locale == 'en' ? '/en' : ''}/blog/${
+      link: `https://correlaid.org${params.locale == 'en' ? '/en' : ''}/blog/${
         post.slug
       }`,
       custom_elements: [{languages: post.langs.join(', ')}],
     });
+  }
 
-  return new Response(feed.xml({indent: true}));
+  return new Response(feed.rss2({indent: true}));
 }
