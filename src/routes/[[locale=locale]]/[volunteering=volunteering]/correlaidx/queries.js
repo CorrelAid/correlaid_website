1ea978c95e6c2b55862876d8bdde6982ae14d78f
jstet <mail@jstet.net>
2024-02-01 15:04:12 +0100

    close #515

--- src/routes/[[locale=locale]]/[volunteering=volunteering]/correlaidx/queries.js
+++ src/routes/[[locale=locale]]/[volunteering=volunteering]/correlaidx/queries.js
@@ -1,6 +1,6 @@
 export const localChapterQuery = `
-query LocalChapters($language: String = "de-DE") {
-	Local_Chapters {
+query LocalChapters($language: String = "de-DE", $status: [String] = ["published"]) {
+	Local_Chapters(filter: { status: { _in: $status } }) {
 		location
 		founded
 		short_id
