5dbc0c73037452c88a0074fb576e6f5a93c8ee92
jstet <mail@jstet.net>
2025-08-04 10:34:21 +0200

    close #892

--- src/routes/[[locale=locale]]/[about=about]/[values=values]/+page.svelte
+++ src/routes/[[locale=locale]]/[about=about]/[values=values]/+page.svelte
@@ -1,19 +1,8 @@
 <script>
   import {pageKey} from '$lib/stores/pageKey';
   import {onMount} from 'svelte';
-  import Person from '$lib/components/Person.svelte';
 
   onMount(() => {
     $pageKey = 'navbar.about.values';
   });
-
-  let {data} = $props();
-
-  let ethicsCommission = $derived(data.ethicsCommission);
 </script>
-
-<div class="container mx-auto flex flex-col gap-y-8 px-4">
-  {#each ethicsCommission as person}
-    <Person {...person} />
-  {/each}
-</div>
