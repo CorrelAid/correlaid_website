a64566278117b4e8b20ac1f336b690ca8a0756cf
jstet <mail@jstet.net>
2025-10-24 11:43:35 +0200

    add new twingle widget

--- src/routes/[[locale=locale]]/[support=support]/[donation=donation]/+page.svelte
+++ src/routes/[[locale=locale]]/[support=support]/[donation=donation]/+page.svelte
@@ -11,7 +11,7 @@
 <div class="container mx-auto">
   <iframe
     title="Twingle Widget"
-    src="https://spenden.twingle.de/correlaid/foerdermitgliedschaft/tw674ef6dbb8cff/form"
+    src="https://spenden.twingle.de/correlaid/spenden-subpage/tw68fb492481ea4/form"
     style="width: 100%; border: none; overflow: hidden; height: 450px; position: relative; z-index: 2;"
     allow="payment"
   ></iframe>
